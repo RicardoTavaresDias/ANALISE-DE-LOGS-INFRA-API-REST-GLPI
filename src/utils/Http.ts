1638c0839832986a60557da22f5acb081be2956e
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-09-27 15:51:51 -0300

    version 1.0.1.7

--- src/utils/Http.ts
+++ src/utils/Http.ts
@@ -1,17 +1,37 @@
 import { env } from "@/config/env"
 
+/**
+ * Interface que define os parâmetros para uma requisição HTTP.
+ */
+
 interface IHttp {
   endpoint: string 
   method: string
   content?: object
 }
 
+/**
+ * Classe responsável por realizar requisições HTTP para a API do GLPI.
+ */
+
 export class Http {
   private session: string
+
+    /**
+   * Cria uma nova instância do cliente HTTP.
+   * 
+   * @param session - Token de sessão válido para autenticação no GLPI.
+   */
 
   constructor (session: string) {
     this.session = session
   }
+
+    /**
+   * Realiza uma requisição à API GLPI
+   * @param params Dados da requisição
+   * @returns Resposta da API em JSON
+   */
 
   async request ({ endpoint, method, content }: IHttp) {
     const response = await fetch(`${env.URLGLPI}${endpoint}`, {
